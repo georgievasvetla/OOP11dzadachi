3b915aa5403a3af1be67de9113e39be5b3a55a34
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-02-15 11:12:36 +0200

    first commit

---
+++ ListCar/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCar
+{
+    internal class Car:IComparable<Car>,IPrint
+    {
+        private string marka;
+        private string regN;
+        private double price;
+        public string Marka
+        {
+            get { return marka; }
+            set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("Grephka");
+                marka = value;
+            }
+   
+        }
+        public string RegN
+        {
+            get { return regN; }
+            set
+            {
+                if (value.Length < 5)
+                    throw new ArgumentException("opitaj pak");
+                regN = value;
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if(value<0)
+                    throw new ArgumentException("Grewka");
+                price = value;
+            }
+
+        }
+        public Car(string marka,string regN,double price)
+        {
+            this.marka = marka;
+            this.regN = regN;
+            this.price = price;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"marka={this.marka}"  +
+                $"regN={this.regN}  price={this.price}");
+        }
+
+        public int CompareTo(Car order)
+        {
+            return marka.CompareTo(order.marka);
+        }
+    }
+}
---
+++ ListCar/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListCar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("count=");
+            int count=int.Parse(Console.ReadLine());
+            List<Car> carList = new List<Car>();
+            for (int i = 0; i < count; i++)
+            {
+                var car = Console.ReadLine().Split();
+                Car cars = new Car(car[0], car[1], double.Parse(car[2]));
+                carList.Add(cars);
+            }
+            carList.Sort();
+            foreach (var item in carList)
+            {
+                item.Print();
+            }
+           
+            
+        }
+        
+    }
+}
